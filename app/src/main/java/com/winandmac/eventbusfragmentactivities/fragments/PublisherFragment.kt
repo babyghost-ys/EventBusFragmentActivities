cc1802ef385176f3302350c515bf9a7d7a21df48
Peter <babyghost@gmail.com>
2022-11-07 23:43:23 -0600

    Added the EventBus event in the fragments.

--- app/src/main/java/com/winandmac/eventbusfragmentactivities/fragments/PublisherFragment.kt
+++ app/src/main/java/com/winandmac/eventbusfragmentactivities/fragments/PublisherFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.winandmac.eventbusfragmentactivities.CustomMessageEvent
 import com.winandmac.eventbusfragmentactivities.R
 import com.winandmac.eventbusfragmentactivities.databinding.FragmentPublisherBinding
+import org.greenrobot.eventbus.EventBus
 
 
 class PublisherFragment : Fragment(R.layout.fragment_publisher) {
@@ -16,5 +18,10 @@ class PublisherFragment : Fragment(R.layout.fragment_publisher) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentPublisherBinding.bind(view)
+        binding.notifyButton.setOnClickListener{
+            val messageToSend = binding.inputMessage.text.toString()
+            val messageEventToSend: CustomMessageEvent = CustomMessageEvent(messageToSend)
+            EventBus.getDefault().postSticky(messageEventToSend)
+        }
     }
 }
--- app/src/main/java/com/winandmac/eventbusfragmentactivities/fragments/SubscriberAFragment.kt
+++ app/src/main/java/com/winandmac/eventbusfragmentactivities/fragments/SubscriberAFragment.kt
@@ -1,12 +1,18 @@
 package com.winandmac.eventbusfragmentactivities.fragments
 
+import android.media.metrics.Event
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.winandmac.eventbusfragmentactivities.CustomMessageEvent
 import com.winandmac.eventbusfragmentactivities.R
 import com.winandmac.eventbusfragmentactivities.databinding.FragmentSubscriberABinding
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class SubscriberAFragment : Fragment(R.layout.fragment_subscriber_a) {
 
@@ -15,6 +21,23 @@ class SubscriberAFragment : Fragment(R.layout.fragment_subscriber_a) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSubscriberABinding.bind(view)
+    }
 
+    // EventBus - Subscribe to the event
+    override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this)
+    }
+
+    // EventBus - Unsubscribe from the event
+    override fun onDestroy() {
+        super.onDestroy()
+        EventBus.getDefault().unregister(this)
+    }
+
+    // EventBus - Receive the event
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(event: CustomMessageEvent) {
+        binding.textOutput.text = event.message
     }
 }
